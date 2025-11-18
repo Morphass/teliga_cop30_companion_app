3d444af79ac114fdd50832eee26b65c075dd268e
Arthur <arthur.rodriguessil21@gmail.com>
2025-11-18 20:02:47 -0300

    fix no render pt2

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -162,7 +162,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # --------------------------------------------------------------------------
 CORS_ALLOWED_ORIGINS = config(
     "CORS_ALLOWED_ORIGINS",
-    default="https://localhost,http://localhost:8000,http://127.0.0.1:8000",
+    default="https://localhost,http://localhost:9000,http://127.0.0.1:9000,http://127.0.0.1:8000",
     cast=lambda v: v.split(",")
 )
 CORS_ALLOW_CREDENTIALS = True
@@ -172,7 +172,7 @@ CORS_ALLOW_CREDENTIALS = True
 # --------------------------------------------------------------------------
 CSRF_TRUSTED_ORIGINS = config(
     "CSRF_TRUSTED_ORIGINS",
-    default="https://localhost,http://localhost:8000,http://127.0.0.1:8000,https://teliga-backend.onrender.com",
+    default="https://localhost,http://localhost:9000,http://127.0.0.1:9000,https://teliga-backend.onrender.com",
     cast=lambda v: v.split(",")
 )
 
@@ -253,3 +253,6 @@ SIMPLE_JWT = {
     "BLACKLIST_AFTER_ROTATION": True,
     "AUTH_HEADER_TYPES": ("Bearer",),
 }
+
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+USE_X_FORWARDED_HOST = True
